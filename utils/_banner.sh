bd72fb41818d5c8c64069f98c17797baac35de6f
Emerson Torres <36451877+Apemerson@users.noreply.github.com>
2025-01-28 17:00:34 -0300

    Instalador Ticket
    Instalador Ticket

---
+++ utils/_banner.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  printf "${GREEN}";
+  printf "                                                     ▄▄█▀▀▀▀▀▀▀█▄▄  \n";
+  printf "                                                   ${GREEN}▄█▀${NC}   ${WHITE}▄▄${NC}      ${GREEN}▀█▄\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}███${NC}         ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}██▄         ${GREEN}█${NC}\n";
+  printf "                                                   ${GREEN}█${NC}     ${WHITE}▀██▄${NC} ${WHITE}██${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}       ${WHITE}▀███▀${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}▀█▄           ▄█▀\n";
+  printf "                                                    ▄█    ▄▄▄▄█▀▀  \n";
+  printf "                                                    █  ▄█▀        \n";
+  printf "                                                    ▀▀▀▀          \n";
+  printf "${NC}";
+
+  printf "\n"
+
+printf "${CYAN_LIGHT}";
+
+
+printf ${CYAN_LIGHT}" ★彡[̲V̲̲A̲̲M̲̲O̲̲S̲ ̲C̲̲O̲̲M̲̲E̲Ç̲A̲̲R̲!!!]彡★\n\n";
+printf ${CYAN_LIGHT}"          ░▄░█░░░▄▀▀▀▀▀▄░░░█░▄░\n";
+printf ${CYAN_LIGHT}"          ▄▄▀▄░░░█─▀─▀─█░░░▄▀▄▄\n";
+printf ${CYAN_LIGHT}"          ░░░░▀▄▒▒▒▒▒▒▒▒▒▄▀░░░░\n";
+printf ${CYAN_LIGHT}"          ░░░░░█────▀────█░░░░░\n";
+printf ${CYAN_LIGHT}"          ░░░░░█────▀────█░░░░░\n\n";
+printf ${CYAN_LIGHT}"       ★★★★彡ＴＯＲＲＥＳ彡★★★★\n";
+                                                                                                                                                         
+  printf " \033[1;33m © Torres - https://www.emersontorres.com.br \n";
+  printf "${NC}";
+
+
+
+  printf "${GREEN}";
+  printf "SISTEMA DE MULTIATENDIMENTO Torres Ticket \n";
+  printf "SUPORTE: +55 43 99655-2155  \n";
+  printf "${NC}";
+
+  printf "\n"
+}
